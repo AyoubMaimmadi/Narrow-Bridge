de8549fc3a6f04856a17260d3c5225936dd659c0
Ayoub Maimmadi <96801986+AyoubMaimmadi@users.noreply.github.com>
2022-10-17 17:10:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -156,21 +156,4 @@ def main():
     
     
 if __name__ == '__main__':
-    main()
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
+    main()  
